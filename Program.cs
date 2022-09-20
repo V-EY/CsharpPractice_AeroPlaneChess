c4abb424f008bb270b31953019f9949cdbe4fd03
旭 张 <360901711@qq.com>
2022-09-20 18:13:56 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CsharpPractice_AeroPlaneChess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
